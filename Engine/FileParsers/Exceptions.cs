6b8ea2a5266e3f3a5d3887d0be5da2aa37d792d2
John Afolayan <jnafolayan@gmail.com>
2021-08-10 11:27:29 +0100

    configure docfx

--- Engine/FileParsers/Exceptions.cs
+++ Engine/FileParsers/Exceptions.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace Axle.Engine.FileParsers.Exceptions
 {
+    /// <summary>
+    /// Thrown when the file was not found
+    /// </summary>
     public class FileNotFoundException : Exception
     {
         public FileNotFoundException(string uri) : base($"File not found: {uri}")
