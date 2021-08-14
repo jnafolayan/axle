0009bb636f56f44481cde23611fabeec34941dab
Anene Praise <anenepraise1@gmail.com>
2021-08-14 15:04:09 +0100

    reformatted and added xml comments

--- Engine/FileParsers/FileParserBase.cs
+++ Engine/FileParsers/FileParserBase.cs
@@ -10,7 +10,7 @@ namespace Axle.Engine.FileParsers
         /// <summary>
         /// Parses a local file into text
         /// </summary>
-        /// <param name="filepath">The path to the file</param>
+        /// <param name="filePath">The path to the file</param>
         /// <returns>The text contained in the file</returns>
         public virtual string ParseLocalFile(string filePath) 
         {
