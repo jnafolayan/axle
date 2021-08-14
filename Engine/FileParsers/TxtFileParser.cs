70e353afbc8b7aed717cffa2b8a689b9979ef86c
Anene Praise <anenepraise1@gmail.com>
2021-08-14 21:04:15 +0100

    finished txt file parser

---
+++ Engine/FileParsers/TxtFileParser.cs
@@ -0,0 +1,26 @@
+using System.IO;
+
+namespace Axle.Engine.FileParsers
+{
+    /// <summary>
+    /// Parser for TXT files
+    /// </summary>
+    public class TxtFileParser : FileParserBase
+    {
+        /// <summary>
+        /// Parses a TXT file and returns the contents as a string
+        /// </summary>
+        /// <param name="filePath">The path to the file</param>
+        /// <returns>The text contained in the file as a string</returns>
+        public override string ParseLocalFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File not found: {filePath}");
+            }
+
+            string text = System.IO.File.ReadAllText(filePath);
+            return text;
+        }
+    }
+}
